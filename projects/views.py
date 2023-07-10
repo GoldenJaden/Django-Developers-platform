e48c0e7a6c9e30b1bfc3ced7313f8e4581098f87
Gusev Yaroslav <yaroslav-gusev-00@mail.ru>
2023-07-11 00:49:59 +0700

    second commit

---
+++ projects/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Project
+
+projectsList = [
+    {
+        'id':'1',
+        'title':"Ecommerce Website",
+        'description':'Fully functional ecommerce website'
+    },
+    {
+        'id':'2',
+        'title':"Portfolio Website",
+        'description':'This was a project where I built out my portfolio'
+    },
+    {
+        'id':'3',
+        'title':"Social Network",
+        'description':"Awesome open source project I am still working on"
+    }
+]
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects':projects}
+    return render(request, 'projects/projects.html', context)
+
+def project(request, pk):
+    projectObj = Project.objects.get(id=pk)
+    context = {'project':projectObj}
+    return render(request, 'projects/single-project.html', context)
